d65ee0bf5effb22cb80560a8f47e3ba6fe58eba2
Inna Mon <inna.mon@gmail.com>
2019-11-11 14:39:23 -0600

    practial component excerise where we insert Bulma message code and add interactivity to it using Vue.component

--- main.js
+++ main.js
@@ -28,6 +28,38 @@ Vue.component('task', {
 
 });
 
+Vue.component('message', {
+    props: ['title', 'body'],
+
+    data() {
+        return {
+            isVisible: true
+        }
+    },
+
+    template: `
+        <article class="message" v-show="isVisible">
+
+            <div class="message-header">
+                <p>{{ title }}</p>
+                <button class="delete" aria-label="delete" 
+                @click="hideMessage"></button>
+            </div>
+
+            <div class="message-body">
+                {{ body }}
+            </div>
+
+        </article>
+    `,
+
+    methods: {
+        hideMessage() {
+            this.isVisible = false;
+        }
+    }
+})
+
 
 
 

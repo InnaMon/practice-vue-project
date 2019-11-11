db59ce58b547168bfa703bb9be277ec291a7fdb6
Inna Mon <inna.mon@gmail.com>
2019-11-11 14:17:38 -0600

    components within components

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+//abouve code creates a global component 
+
+//descibes what a task list outputs 
+
+Vue.component('task-list', {
+
+    template: `<div>
+                    <task v-for="task in tasks">{{ task.task }}</task>
+                </div>`,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', complete: false},
+                { task: 'Go to the bank', complete: false},
+                { task: 'Go to work', complete: true}
+            ]
+        }
+    }
+
+});
+
+//describes what a task outputs 
+
 Vue.component('task', {
 
     template: '<li><slot></slot></li>'
 
 });
 
-//abouve code creates a global component 
+
+
 
 new Vue({
     el: '#root'
